a6a48aef4108617c7d9a96079cd41227c553b7e3
John Macaluso <John.Macaluso@infor.com>
2020-03-05 16:33:42 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, createRef } from 'react';
+
+const PROXY = `https://cors-anywhere.herokuapp.com/`;
+const API_URL = `http://api.bely.me`;
+const API_KEY = `5d51945e59e81470096ed99540d1771e`;
+
+class UrlForm extends Component {
+  state = {
+    url: '',
+    urlList: []
+  }
+
+  constructor(props) {
+    super(props)
+    this.urlInput = createRef();
+  }
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const { url } = this.state;
+    const data = { 
+      url, 
+    };
+
+    fetch(`${PROXY}${API_URL}/links`, {
+      method: 'POST',
+      headers: {
+        'GB-Access-Token': `${API_KEY}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data)
+    })
+    .then(blob => blob.json())
+    .then(res => {
+      this.setState({
+        urlList: [...this.state.urlList, res]
+      });
+      
+      this.urlInput.current.value = '';
+    });
+
+
+  }
+
+  render() {
+    const { url, urlList } = this.state;
+
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <input 
+            ref={this.urlInput}
+            type="text" 
+            name="url"
+            value={url}
+            placeholder="Enter URL"
+            onChange={this.onChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <ul>
+          {urlList.map((url, index) => (
+            <li key={index}>{url.short_url}</li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UrlForm />
     </div>
   );
 }
